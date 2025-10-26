36aff4eba0bf32d64df22daa66c35ee9dee5afd2
oddava <javlonbekdeveloper@gmail.com>
2025-10-26 17:13:52 +0500

    fixed i18 middleware issue

--- bot/middlewares/i18n.py
+++ bot/middlewares/i18n.py
@@ -8,9 +8,9 @@ from typing import Any, Dict
 class CustomI18nMiddleware(I18nMiddleware):
     async def get_locale(self, event: TelegramObject, data: Dict[str, Any]) -> str:
         user = await User.get(data.get("event_from_user").id)
-        user_language = None
-
-        if user is not None:
-            user_language = user.language
+        try:
+            user_language = user.language_code
+        except AttributeError:
+            user_language = False
 
         return user_language or data.get("event_from_user").language_code or "en"
