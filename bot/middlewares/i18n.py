5d96308c6b95087d46f67d960e52b040edad11f2
oddava <javlonbekdeveloper@gmail.com>
2025-10-26 17:09:05 +0500

    fixed i18 middleware issue

--- bot/middlewares/i18n.py
+++ bot/middlewares/i18n.py
@@ -4,7 +4,13 @@ from aiogram.utils.i18n.middleware import I18nMiddleware
 from aiogram.types import TelegramObject
 from typing import Any, Dict
 
+
 class CustomI18nMiddleware(I18nMiddleware):
     async def get_locale(self, event: TelegramObject, data: Dict[str, Any]) -> str:
         user = await User.get(data.get("event_from_user").id)
-        return user.language_code or data.get("event_from_user").language_code or "en"
+        user_language = None
+
+        if user is not None:
+            user_language = user.language
+
+        return user_language or data.get("event_from_user").language_code or "en"
